b41f8f4cbbb76110a8299e597484c93a0765cb3d
Guillaume Bougard <gbougard@teclib.com>
2023-05-16 15:18:07 +0200

    build: Set version from tag when publishing Always to upload artifacts, even on failure Set internal filename to include arch to match released names

---
+++ version.h
@@ -0,0 +1,5 @@
+// File overridden during GH Actions workflow run
+#define VI_FILENAME         "GLPI-AgentMonitor.exe"
+#define VI_VERSIONDEF       1,0,0,0
+#define VI_VERSIONSTRING    "1.0.0.0"
+#define VI_PRODUCTNAME      "GLPI Agent Monitor"
