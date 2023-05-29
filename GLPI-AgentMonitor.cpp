e964fd43ace7fe8b5cf0c2192aa334129c5615d7
Leonardo Bernardes <58712628+redddcyclone@users.noreply.github.com>
2023-05-29 11:28:17 -0300

    Wrong comparison for the error value
    This was left accidentally when testing.

--- GLPI-AgentMonitor.cpp
+++ GLPI-AgentMonitor.cpp
@@ -497,7 +497,7 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance
             ControlService(hAgentSvc, SERVICE_CONTROL_STOP, &svcStatus);
         else if (wcsstr(lpCmdLine, L"/continueSvc") != nullptr)
             ControlService(hAgentSvc, SERVICE_CONTROL_CONTINUE, &svcStatus);
-        if (dwErr == NULL) {
+        if (dwErr != NULL) {
             dwErr = GetLastError();
             LoadStringAndMessageBox(hInst, NULL, IDS_ERR_SVCOPERATION, IDS_ERROR, MB_OK | MB_ICONERROR, dwErr);
             return dwErr;
