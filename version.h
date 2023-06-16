921ba7b26953ca660dfd98719f3260c0b8c3b531
Guillaume Bougard <gbougard@teclib.com>
2023-06-16 17:40:01 +0200

    Bump 1.2.2 version

--- version.h
+++ version.h
@@ -1,5 +1,5 @@
 // File overridden during GH Actions workflow run
 #define VI_FILENAME         "GLPI-AgentMonitor.exe"
-#define VI_VERSIONDEF       1,2,1,0
-#define VI_VERSIONSTRING    "1.2.1.0"
+#define VI_VERSIONDEF       1,2,2,0
+#define VI_VERSIONSTRING    "1.2.2.0"
 #define VI_PRODUCTNAME      "GLPI Agent Monitor"
