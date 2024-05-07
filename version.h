49efcbec3475bb13e66dd5aaff4567b4375e2ff3
Guillaume Bougard <gbougard@teclib.com>
2024-05-07 10:36:29 +0200

    New 1.3.0 version

--- version.h
+++ version.h
@@ -1,5 +1,5 @@
 // File overridden during GH Actions workflow run
 #define VI_FILENAME         "GLPI-AgentMonitor.exe"
-#define VI_VERSIONDEF       1,2,3,0
-#define VI_VERSIONSTRING    "1.2.3.0"
+#define VI_VERSIONDEF       1,3,0,0
+#define VI_VERSIONSTRING    "1.3.0.0"
 #define VI_PRODUCTNAME      "GLPI Agent Monitor"
